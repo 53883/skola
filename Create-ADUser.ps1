c18f95129667d1ebff0c508384529bbf5cea14b3
53883 <46386707+53883@users.noreply.github.com>
2019-01-13 22:06:15 +0100

    Kommentarer, ny $ou variabel, tagit bort etc

--- Create-ADUser.ps1
+++ Create-ADUser.ps1
@@ -1,10 +1,19 @@
-#region company variables
+#region COMPANY VARIABLES
+
+# IP to AD-Server
 $adip = "192.168.1.206"
+
+# Domain 
 $company = "test.local"
+
+# The OU where the users are
+$ou = "OU=Users,OU=test,DC=test,DC=local"
+
 #$credentials = Get-Credential
 #endregion
 
-#region functions
+#region FUNCTIONS
+# Removes specialcharacters
 function Remove-StringLatinCharacters
 {
     PARAM ([string]$String)
@@ -12,29 +21,32 @@ function Remove-StringLatinCharacters
 }
 #endregion
 
-# Skapa en användare
-
+#region VARIABLES
 $name = Read-Host -Prompt "First and lastname: "
-while ($name.Split(" ").Length -lt 2 -or $name.Split(" ").Length -gt 3) {
+while ($name.Split(" ").Length -ne 2) {
     Write-Output "Invalid input"
     $name = Read-Host -Prompt "First and lastname: "
 }
-if ($name.Split(" ").Length -gt 2) {
 
-}
 $firstname = $name.Split(" ")[0]
 $lastname = $name.Split(" ")[1]
 $username = (Remove-StringLatinCharacters -String ($firstname.Substring(0,3).ToLower() + $lastname.Substring(0,3).ToLower()))
 $password = [System.Web.Security.Membership]::GeneratePassword(10,0) | ConvertTo-SecureString -AsPlainText -Force
 $email = (Remove-StringLatinCharacters -String "$($name.Replace(" ", ".").ToLower())@$($company)")
 $phonenr = Read-Host -Prompt "Phone number: "
-$location = Get-ADOrganizationalUnit -LDAPFilter "(name=*)" -SearchBase "OU=Users,OU=test,DC=test,DC=local" -SearchScope OneLevel -Server $adip -Credential $credentials | Select -Property "Name", "DistinguishedName" | Out-GridView -PassThru
+$location = Get-ADOrganizationalUnit -LDAPFilter "(name=*)" -SearchBase $ou -SearchScope OneLevel -Server $adip -Credential $credentials | Select-Object -Property "Name", "DistinguishedName" | Out-GridView -PassThru
 $counter = 0
+
+# Check if sAMAccountName already exists
+# Note to self, check userprinciplename?
 while ((Get-ADUser -Filter "sAMAccountName -eq '$($username)'" -Server $adip -Credential $credentials)) {
     Write-Output "User already exists"
     $counter += 1
     $username = "$username$counter"
 }
+#endregion
+
+#region CREATE USER
 New-ADUser -Name $name `
      -SamAccountName $username `
      -DisplayName $name `
@@ -48,4 +60,9 @@ New-ADUser -Name $name `
      -ChangePasswordAtLogon $true `
      -Enabled $true `
      -Server $adip `
-     -Credential $credentials
+-Credential $credentials
+#endregion
+
+#region output to user
+Write-Host "Your User is created: `n Name: $name `n Email: $email `n Login: $username `n Password: $password"
+#endregion
