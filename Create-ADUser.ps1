69d04105d61c03520e1b0736afcddffde0a58dbe
53883 <46386707+53883@users.noreply.github.com>
2019-01-13 16:41:30 +0100

    Kolla om användare finns

--- Create-ADUser.ps1
+++ Create-ADUser.ps1
@@ -14,10 +14,10 @@ function Remove-StringLatinCharacters
 
 # Skapa en användare
 
-$name = Read-Host -Prompt "F\ör- och efternamn: "
+$name = Read-Host -Prompt "First and lastname: "
 while ($name.Split(" ").Length -lt 2 -or $name.Split(" ").Length -gt 3) {
-    Write-Output "Felaktig input."
-    $name = Read-Host -Prompt "F\ör- och efternamn: "
+    Write-Output "Invalid input"
+    $name = Read-Host -Prompt "First and lastname: "
 }
 if ($name.Split(" ").Length -gt 2) {
 
@@ -27,15 +27,14 @@ $lastname = $name.Split(" ")[1]
 $username = (Remove-StringLatinCharacters -String ($firstname.Substring(0,3).ToLower() + $lastname.Substring(0,3).ToLower()))
 $password = [System.Web.Security.Membership]::GeneratePassword(10,0) | ConvertTo-SecureString -AsPlainText -Force
 $email = (Remove-StringLatinCharacters -String "$($name.Replace(" ", ".").ToLower())@$($company)")
-$phonenr = Read-Host -Prompt "Telefonnummer tack: "
+$phonenr = Read-Host -Prompt "Phone number: "
 $location = Get-ADOrganizationalUnit -LDAPFilter "(name=*)" -SearchBase "OU=Users,OU=test,DC=test,DC=local" -SearchScope OneLevel -Server $adip -Credential $credentials | Select -Property "Name", "DistinguishedName" | Out-GridView -PassThru
-
-<# while (Get-ADUser -Identity $username -Server $adip -Credential $credentials) {
-    Write-Output "Användare med det användarnamet finns redan."
+$counter = 0
+while ((Get-ADUser -Filter "sAMAccountName -eq '$($username)'" -Server $adip -Credential $credentials)) {
+    Write-Output "User already exists"
     $counter += 1
-    $username = "$username + $counter"
-} #>
-
+    $username = "$username$counter"
+}
 New-ADUser -Name $name `
      -SamAccountName $username `
      -DisplayName $name `
