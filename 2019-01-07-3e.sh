01547edeb0fac891dc3381c4ccfa4bfee8593174
53883 <46386707+53883@users.noreply.github.com>
2019-01-19 17:55:09 +0100

    Fått fram de individuella siffrorna

--- 2019-01-07-3e.sh
+++ 2019-01-07-3e.sh
@@ -1,44 +1,51 @@
-#!/bin/bash
-
 min=1000
 max=10000
 luck=7
 
-number=9876
+number=62431
 echo "$number start"
 
 broken=$(( $number % 10 ))
 echo "$broken Last digit"
 
 number=$(( $number / 10 ))
-echo "$number whats left"
+#echo "$number whats left"
 
 summa=$(( $summa + $broken ))
-echo "$summa summan"
+#echo "$summa summan"
 
 broken=$(( $number % 10 ))
 echo "$broken Last digit 2"
 
 number=$(( $number / 10 ))
-echo "$number whats left"
+#echo "$number whats left"
 
 summa=$(( $summa + $broken ))
-echo "$summa summan 1+2"
+#echo "$summa summan 1+2"
 
 broken=$(( $number % 10 ))
-echo "$broken Last digit 2"
+echo "$broken Last digit 3"
 
 number=$(( $number / 10 ))
-echo "$number whats left"
+#echo "$number whats left"
 
 summa=$(( $summa + $broken ))
-echo "$summa summan 1+2+3"
+#echo "$summa summan 1+2+3"
 
 broken=$(( $number % 10 ))
-echo "$broken Last digit 2"
+echo "$broken Last digit 4"
 
 number=$(( $number / 10 ))
-echo "$number whats left"
+#echo "$number whats left"
 
 summa=$(( $summa + $broken ))
-echo "$summa summan 1+2+3"
+#echo "$summa summan 1+2+3+4"
+
+broken=$(( $number % 10 ))
+echo "$broken Last digit 5"
+
+number=$(( $number / 10 ))
+#echo "$number whats left"
+
+summa=$(( $summa + $broken ))
+#echo "$summa summan 1+2+3+4+5"
