52d683a87589e8e8fdbb0c3bd065e26e52124922
53883 <46386707+53883@users.noreply.github.com>
2019-01-19 23:57:42 +0100

    Update 2019-01-07-3e.sh

--- 2019-01-07-3e.sh
+++ 2019-01-07-3e.sh
@@ -1,51 +1,19 @@
+#!/bin/bash
+
 min=1000
 max=10000
 luck=7
-
 number=62431
-echo "$number start"
+echo $number
+while [ $number != 0 ]
+do
+        broken=$(( $number % 10 ))
+        echo "$broken Last digit"
 
-broken=$(( $number % 10 ))
-echo "$broken Last digit"
+        number=$(( $number / 10 ))
+        #echo "$number whats left"
 
-number=$(( $number / 10 ))
-#echo "$number whats left"
+        summary=$(( $summary + $broken ))
+        #echo "$summary summan"
+done
 
-summa=$(( $summa + $broken ))
-#echo "$summa summan"
-
-broken=$(( $number % 10 ))
-echo "$broken Last digit 2"
-
-number=$(( $number / 10 ))
-#echo "$number whats left"
-
-summa=$(( $summa + $broken ))
-#echo "$summa summan 1+2"
-
-broken=$(( $number % 10 ))
-echo "$broken Last digit 3"
-
-number=$(( $number / 10 ))
-#echo "$number whats left"
-
-summa=$(( $summa + $broken ))
-#echo "$summa summan 1+2+3"
-
-broken=$(( $number % 10 ))
-echo "$broken Last digit 4"
-
-number=$(( $number / 10 ))
-#echo "$number whats left"
-
-summa=$(( $summa + $broken ))
-#echo "$summa summan 1+2+3+4"
-
-broken=$(( $number % 10 ))
-echo "$broken Last digit 5"
-
-number=$(( $number / 10 ))
-#echo "$number whats left"
-
-summa=$(( $summa + $broken ))
-#echo "$summa summan 1+2+3+4+5"
