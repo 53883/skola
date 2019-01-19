7f4dbb0eb125d06f6c7c58ddb94afe308db4b0b4
53883 <46386707+53883@users.noreply.github.com>
2019-01-19 17:50:24 +0100

    Update 2019-01-07-3e.sh

--- 2019-01-07-3e.sh
+++ 2019-01-07-3e.sh
@@ -5,15 +5,40 @@ max=10000
 luck=7
 
 number=9876
+echo "$number start"
 
 broken=$(( $number % 10 ))
-echo $broken
+echo "$broken Last digit"
 
 number=$(( $number / 10 ))
-echo $number
+echo "$number whats left"
 
 summa=$(( $summa + $broken ))
-echo $summa
+echo "$summa summan"
 
 broken=$(( $number % 10 ))
-echo $broken
+echo "$broken Last digit 2"
+
+number=$(( $number / 10 ))
+echo "$number whats left"
+
+summa=$(( $summa + $broken ))
+echo "$summa summan 1+2"
+
+broken=$(( $number % 10 ))
+echo "$broken Last digit 2"
+
+number=$(( $number / 10 ))
+echo "$number whats left"
+
+summa=$(( $summa + $broken ))
+echo "$summa summan 1+2+3"
+
+broken=$(( $number % 10 ))
+echo "$broken Last digit 2"
+
+number=$(( $number / 10 ))
+echo "$number whats left"
+
+summa=$(( $summa + $broken ))
+echo "$summa summan 1+2+3"
