5fff875245c6a73ae771199f7322102ba6fd4053
53883 <46386707+53883@users.noreply.github.com>
2019-01-20 20:44:33 +0100

    And added the final IF

--- 2019-01-07-3e.sh
+++ 2019-01-07-3e.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-min=1000
-max=10000
+min=9000
+max=9050
 min2=$min
 summary=0
 
@@ -12,12 +12,22 @@ do
                 broken=$(( $min % 10 ))
                 min=$(( $min / 10 ))
                 summary=$(( $summary + $broken ))
+#echo $summary
         done
 
         if [ "$summary" = "7" ] 
         then    
                 echo "$min2"
-        fi      
+        else
+                a=$(( $summary % 10 ))
+                b=$(( $summary / 10 ))
+                c=$(( $a + $b ))
+
+                        if [ "$c" = "7" ]
+                        then
+                                echo "$min2"
+                        fi
+        fi
  summary=0
  min2=$((min2 + 1))
  min=$min2
