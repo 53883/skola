d245431675aa078133983e14c53249aacfd7ec6d
53883 <46386707+53883@users.noreply.github.com>
2019-01-20 20:52:46 +0100

    Finishing up. Done (I hope)

--- 2019-01-07-3e.sh
+++ 2019-01-07-3e.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 
-min=9000
-max=9050
+# VARIABLES #
+min=1000
+max=10000
 min2=$min
 summary=0
+luck=7
 
+# FIRST WHILE LOOP #
+# counts to 10000 then stops #
 while [ "$min" != "$max" ]
 do
-        while [ "$min" != "0" ]
+        while [ "$min" != "0" ] # 2ND WHILE # Stops when hitting 0 #
         do  
-                broken=$(( $min % 10 ))
-                min=$(( $min / 10 ))
-                summary=$(( $summary + $broken ))
-#echo $summary
+                broken=$(( $min % 10 )) # Modulus to remove last digit #
+                min=$(( $min / 10 )) # remove last digit from startvalue #
+                summary=$(( $summary + $broken )) # add all the sums #
         done
 
-        if [ "$summary" = "7" ] 
-        then    
+        # IF summary is 7, then print whole number #
+        if [ "$summary" = "$luck" ]
+        then
                 echo "$min2"
-        else
+        else # If not 7, calc if numbers get to 7 (1+3+3=7 for example) #
                 a=$(( $summary % 10 ))
                 b=$(( $summary / 10 ))
                 c=$(( $a + $b ))
 
-                        if [ "$c" = "7" ]
+                        if [ "$c" = "$luck" ]
                         then
                                 echo "$min2"
                         fi
         fi
+
+# VARIABLES INSIDE THE WHILE LOOP #     
  summary=0
- min2=$((min2 + 1))
+ min2=$(( min2 + 1 ))
  min=$min2
 done
