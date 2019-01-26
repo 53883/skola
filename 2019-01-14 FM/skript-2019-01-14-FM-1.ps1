a70256a25a46424428a3eb718dc6f872367c0a55
53883 <46386707+53883@users.noreply.github.com>
2019-01-26 23:35:01 +0100

    Update skript-2019-01-14-FM-1.ps1

--- 2019-01-14 FM/skript-2019-01-14-FM-1.ps1
+++ 2019-01-14 FM/skript-2019-01-14-FM-1.ps1
@@ -6,16 +6,18 @@ New-Item -ItemType Directory -Force -Path "C:\temp"
 New-Item -ItemType File -Force -Name "test.txt" -Path "C:\temp"
 
 # Lägg till dagens datum som text i test.txt.
-$datum = $(Get-Date | Out-File -encoding ascii -FilePath "C:\temp\test.txt")
+$datum = $(Get-Date -UFormat "%Y-%m-%d")
+Add-Content -Path C:\temp\test.txt -Value "$datum"
 
 # Lägg in texten "Välkommen till Nackademin" i test.txt.
-$nackademin = $(Add-Content -Path C:\temp\test.txt -Value "Välkommen till Nackademin")
+$nackademin = "Välkommen till Nackademin"
+Add-Content -Path C:\temp\test.txt -Value $nackademin
 
 # Radera innehållet i filen
 Clear-Content "C:\temp\test.txt"
 
 # Lägg in texten "Välkommen till Nackademin" i test.txt.
-Invoke-Expression $nackademin
+Add-Content -Path C:\temp\test.txt -Value "$nackademin"
 
 #Lägg till dagens datum i test.txt.
-& $datum
+Add-Content -Path C:\temp\test.txt -Value "$datum"
