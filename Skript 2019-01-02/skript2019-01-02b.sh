4433fa05865e68b60d1c5a4ec58d4204d7a1af31
53883 <46386707+53883@users.noreply.github.com>
2019-01-26 13:14:48 +0100

    Add files via upload

---
+++ Skript 2019-01-02/skript2019-01-02b.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+file2a="https://github.com/53883/skola/blob/master/skript2019-01-02a.sh"
+folder2="folder2"
+file2b="skript2019-01-02a.sh"
+
+wget -nc $file2a
+mkdir -p $folder2
+mv $file2b $folder2
