fcad7f3e4f27c01b52dec5bd448f51e44bfe06e5
53883 <46386707+53883@users.noreply.github.com>
2019-01-26 13:15:37 +0100

    Delete skript2019-01-02b.sh

--- skript2019-01-02b.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-file2a="https://github.com/53883/skola/blob/master/skript2019-01-02a.sh"
-folder2="folder2"
-file2b="skript2019-01-02a.sh"
-
-wget $file2a
-mkdir -p $folder2
-mv $file2b $folder2
