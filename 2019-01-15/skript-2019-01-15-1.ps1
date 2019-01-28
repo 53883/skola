2683be43adb244de9a02c1024da924f3e8e8a72f
53883 <46386707+53883@users.noreply.github.com>
2019-01-28 22:51:33 +0100

    Add files via upload

---
+++ 2019-01-15/skript-2019-01-15-1.ps1
@@ -0,0 +1,17 @@
+﻿# Skapa certet
+$selfsigncert = New-SelfSignedCertificate `
+    -Subject "CN=Sebastians Signade Cert" `
+    -KeyAlgorithm RSA `
+    -KeyLength 2048 `
+    -Type CodeSigningCert `
+    -CertStoreLocation Cert:\LocalMachine\My\
+# Flytta certet till root
+Move-Item "Cert:\LocalMachine\My\$($selfsigncert.Thumbprint)" Cert:\LocalMachine\Root
+
+
+### OBS ###
+# Kör detta direkt i en PowerShell-prompt med adminbehörigheter
+# Verkar inte gå att köra detta i ISE, så man får öppna powershell och göra det manuellt
+Set-ExecutionPolicy AllSigned
+$cert = gci Cert:\LocalMachine\Root\ -CodeSigningCert | Out-GridView -PassThru
+Set-AuthenticodeSignature -Certificate $cert -FilePath "C:\temp\signerat_skript.ps1"
