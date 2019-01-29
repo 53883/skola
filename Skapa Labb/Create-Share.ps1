e31e6e3bea2b791786347d840e318a1dc3a22311
53883 <46386707+53883@users.noreply.github.com>
2019-01-29 23:13:33 +0100

    Update Create-Share.ps1

--- Skapa Labb/Create-Share.ps1
+++ Skapa Labb/Create-Share.ps1
@@ -12,14 +12,11 @@ $User = Get-ADUser -Identity $samAccountName
 if($User -ne $Null) {
     Set-ADUser $User -HomeDrive $driveLetter -HomeDirectory $fullPath -ea Stop
     $homeShare = New-Item -path $fullPath -ItemType Directory -force -ea Stop
- 
     $acl = Get-Acl $homeShare
- 
     $FileSystemRights = [System.Security.AccessControl.FileSystemRights]"Modify"
     $AccessControlType = [System.Security.AccessControl.AccessControlType]::Allow
     $InheritanceFlags = [System.Security.AccessControl.InheritanceFlags]"ContainerInherit, ObjectInherit"
     $PropagationFlags = [System.Security.AccessControl.PropagationFlags]"InheritOnly"
- 
     $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule ($User.SID, $FileSystemRights, $InheritanceFlags, $PropagationFlags, $AccessControlType)
     $acl.AddAccessRule($AccessRule)
  
